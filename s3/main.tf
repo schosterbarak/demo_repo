811ce9651c2569182f02239458d489b9436a57a4
Barak Schoster Goihman <barak@bridgecrew.io>
2020-01-26 21:07:31 +0200

    fix: s3/main.tf & reduce cloud risk
    The following errors are fixed with an upgrade:
    https://www.bridgecrew.cloud/incidents/BC_AWS_S3_14

--- s3/main.tf
+++ s3/main.tf
@@ -3,4 +3,11 @@ resource "aws_s3_bucket" "acme-s3-bucket" {
   tags = {
     Environment = "Production"
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
